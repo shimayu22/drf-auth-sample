20fbc8000e9a95d28cf6523deaf717f2cab6e346
Yutaka Shimada <shimayu22@gmail.com>
2020-07-16 11:58:00 +0900

    モデル作成

--- sample_auth/models.py
+++ sample_auth/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    memo = models.CharField(
+      verbose_name="TODO",
+      max_length=50,
+    )
+
+    created_at = models.DateTimeField(
+      verbose_name="登録日",
+      auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        verbose_name="更新日",
+        auto_now=True,
+    )
+
+    def __str__(self):
+      return self.memo + " : " + str(self.created_at) + " : " + str(self.updated_at)
